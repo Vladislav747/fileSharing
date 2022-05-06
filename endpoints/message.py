015f6101b9143ebcd0e047b19ecac3fd95229068
Vladislav747 <zippo585@gmail.com>
2022-05-06 23:54:23 +0500

    Updated

--- endpoints/message.py
+++ endpoints/message.py
@@ -13,6 +13,7 @@ router = APIRouter(
 async def root():
     return messages_database
 
+
 @router.get("/{message_id}")
 async def get_message(message_id: int):
     return {"message": messages_database[message_id - 1]}
@@ -22,9 +23,10 @@ async def get_message(message_id: int):
 async def add_message(message: Message):
     message_db = Message(id=len(messages_database) + 1, **message.dict())
     # Поместить запрос в чат
-    x = type(chats_db)
 
-    print(x)
+    for chat in chats_db:
+        if chat["id"] == 1:
+            chat["messages_ids"].append(len(messages_database) + 1)
     return message_db
 
 
@@ -34,6 +36,7 @@ async def update_message(message_id: int, message: Message):
     for param, value in message.dict().items():
         message_db[param] = value
     return message_db
+
 
 @router.delete("/{message_id}", response_model=Message)
 async def del_message(message_id: int):
