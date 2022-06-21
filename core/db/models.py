01ee8d40c09d5cf13706c402d09f2f7ee28bb1f4
Vladislav747 <zippo585@gmail.com>
2022-06-21 15:21:07 +0500

    Updated models

--- core/db/models.py
+++ core/db/models.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users"
         
-    id  = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     login = Column(String)
     password = Column(String)
     name = Column(String(40))
@@ -18,7 +18,8 @@ class Chat(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    created_data = Column(DateTime, server_default=func.now())
+    created_date = Column(DateTime, server_default=func.now())
+    type = Column(String)
 
 class UserChat(Base):
     __tablename__ = "users_chats"
@@ -26,4 +27,27 @@ class UserChat(Base):
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     chat_id = Column(Integer, ForeignKey('chats.id'))
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    message = Column(String)
+    created_date = Column(DateTime, server_default=func.now())
+
+class ChatMessage(Base):
+    __tablename__ = "chats_messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    chat_id = Column(Integer, ForeignKey('chats.id'))
+    message_id = Column(Integer, ForeignKey('messages.id'))
+
+
+class UserMessage(Base):
+    __tablename__ = "users_messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    message_id = Column(Integer, ForeignKey('messages.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
 
